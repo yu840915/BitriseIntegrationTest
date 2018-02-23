bb306308d028db9fa0de78eaad1f5f8464c86a1c
yu840915 <mike@17.media>
2018-02-23 15:38:49 +0800

    Initial Commit

---
+++ BitriseIntegrationTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BitriseIntegrationTest
+//
+//  Created by Mike Yu on 23/02/2018.
+//  Copyright © 2018 yu840915. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
