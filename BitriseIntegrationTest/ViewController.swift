adaad85cd94e4fda3ccaa9e8b0aa10ca31570483
yu840915 <mike@17.media>
2018-02-26 15:10:39 +0800

    Log 2nd line

--- BitriseIntegrationTest/ViewController.swift
+++ BitriseIntegrationTest/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Hello world")
+        print("Have a nice day")
     }
 
     override func didReceiveMemoryWarning() {
